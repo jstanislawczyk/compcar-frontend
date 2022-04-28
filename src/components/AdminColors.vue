2a2c3893c47d89fec7d82303456ad3f5124fd7c7
stanislawczykj <jakubstanislawczyk98@gmail.com>
2022-04-28 20:23:56 +0200

    Fetching colors data

--- src/components/AdminColors.vue
+++ src/components/AdminColors.vue
@@ -5,13 +5,53 @@
 </template>
 
 <script>
+import { parseGraphQlErrorMessage } from '@/common/errors';
+import gql from 'graphql-tag';
+
 export default {
   name: 'AdminColors',
   data() {
     return {
+      colors: [],
     };
   },
+  async created() {
+    await this.setupColorsData();
+  },
   methods: {
+    async setupColorsData() {
+      try {
+        const getAllColorsQuery = this.getAllColorsQuery();
+        const colorsResponse = await this.$apollo.query(getAllColorsQuery);
+
+        const colors = colorsResponse.data.getColors;
+        this.colors = this.buildColors(colors);
+        console.log(this.colors);
+      } catch (error) {
+        const parsedError = parseGraphQlErrorMessage(error);
+        console.log(parsedError);
+      }
+    },
+    getAllColorsQuery() {
+      return {
+        query: gql`
+          {
+            getColors {
+              id,
+              name,
+              hexCode,
+            },
+          }
+        `,
+      };
+    },
+    buildColors(colors) {
+      return colors.map((color) => ({
+        id: color.id,
+        name: color.name,
+        hexCode: color.hexCode,
+      }));
+    },
   },
 };
 </script>
